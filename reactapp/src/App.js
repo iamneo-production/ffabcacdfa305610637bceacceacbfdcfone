1fce53333e4976f72fddc49c4378abb056782d75
Iamneo <techadmin@iamneo.ai>
2023-09-30 05:27:45 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Stopwatch from './components/Stopwatch';
 
 function App() {
   return (
-    
+    <div className="App">
+      <Stopwatch></Stopwatch>
     </div>
   );
 }
--- reactapp/src/components/Stopwatch.js
+++ reactapp/src/components/Stopwatch.js
@@ -1,37 +1,61 @@
-import React, { Component } from 'react';
-import LapItem from './LapItem';
-import './Stopwatch.css';
+import React, { useState, useEffect } from "react";
 
-class Stopwatch extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isRunning: false,
-      startTime: 0,
-      currentTime: 0,
-      laps: [],
-    };
-  }
 
-  // Add your lifecycle methods and event handlers here
+function Stopwatch() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
 
-  render() {
-    return (
-      <div className="stopwatch-container">
-        <h1 className="stopwatch-title">Stopwatch</h1>
-        <div className="time-display">{/* Display formatted time here */}</div>
-        <div>
-          <button className="action-button start-button">Start</button>
-          <button className="action-button stop-button">Stop</button>
-          <button className="action-button reset-button">Reset</button>
-          <button className="action-button add-lap-button">Add Lap</button>
-        </div>
-        <ul className="lap-list">
-          {/* Render lap items here */}
-        </ul>
+  useEffect(() => {
+    let interval;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const handleStartStop = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  const formatTime = (timeInSeconds) => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+
+    const formattedHours = hours.toString().padStart(2, "0");
+    const formattedMinutes = minutes.toString().padStart(2, "0");
+    const formattedSeconds = seconds.toString().padStart(2, "0");
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  };
+
+  return (
+    <div className="stopwatch-container">
+      <div className="stopwatch-display">{formatTime(time)}</div>
+      <div className="stopwatch-buttons">
+        <button
+          className="stopwatch-start-stop-button"
+          onClick={handleStartStop}
+        >
+          {isRunning ? "Stop" : "Start"}
+        </button>
+        <button className="stopwatch-reset-button" onClick={handleReset}>
+          Reset
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Stopwatch;
